f523583392374aca7c2709456fd56de38731c80d
Karthik Shanmugavel <135093193+Karthik220399@users.noreply.github.com>
2024-01-24 05:00:59 +0000

    c

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,7 @@ export default function App() {
           <button
             onClick={() => {
               setInput("");
-              setAnswer();
+              setAnswer("");
             }}
             className="key"
           >
