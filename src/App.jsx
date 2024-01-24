468ed076b5a4a4ccb767c06bb1a9890a856c4327
Karthik Shanmugavel <135093193+Karthik220399@users.noreply.github.com>
2024-01-24 04:51:33 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [input, setInput] = useState("");
+  const [answer, setAnswer] = useState();
+
+  const update = (e) => {
+    setInput((prev) => {
+      return prev + e.target.innerText;
+    });
+  };
+  return (
+    <div key={"m11"} className="container">
+      <h1>React Calculator</h1>
+      <input defaultValue={input} />
+      {answer ? <div className="answer">{answer}</div> : null}
+      <div className="keyContainer">
+        <div className="keypad">
+          <button onClick={update} className="key">
+            7
+          </button>
+          <button onClick={update} className="key">
+            8
+          </button>
+          <button onClick={update} className="key">
+            9
+          </button>
+          <button onClick={update} className="key">
+            +
+          </button>
+
+          <button onClick={update} className="key">
+            4
+          </button>
+          <button onClick={update} className="key">
+            5
+          </button>
+          <button onClick={update} className="key">
+            6
+          </button>
+          <button onClick={update} className="key">
+            -
+          </button>
+
+          <button onClick={update} className="key">
+            1
+          </button>
+          <button onClick={update} className="key">
+            2
+          </button>
+          <button onClick={update} className="key">
+            3
+          </button>
+          <button onClick={update} className="key">
+            *
+          </button>
+
+          <button
+            onClick={() => {
+              setInput("");
+              setAnswer();
+            }}
+            className="key"
+          >
+            C
+          </button>
+          <button onClick={update} className="key">
+            0
+          </button>
+          <button
+            onClick={() => {
+              if (input.length <= 0) {
+                setAnswer("Error");
+              } else {
+                let ans = eval(input);
+                setAnswer(ans);
+              }
+            }}
+            className="key"
+          >
+            =
+          </button>
+          <button onClick={update} className="key">
+            /
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
